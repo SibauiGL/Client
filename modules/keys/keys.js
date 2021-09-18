adcea292a89ec30053e374e9ecb773896b123fa6
Dschahannam <uwunoozles@gmail.com>
2021-09-18 13:11:18 +0200

    Add Dot and Comma Keybinds

--- modules/keys/keys.js
+++ modules/keys/keys.js
@@ -7,8 +7,9 @@ class Keys {
     constructor() {
         let lastPressedE = 0;
         let lastPressedI = 0;
+        let lastInteractKey = 0
 
-        mp.keys.bind(0x45, false, function() {
+        mp.keys.bind(0x45, true, function() {
             if (!player.loaded) return;
 
             const now = Date.now()
@@ -19,7 +20,7 @@ class Keys {
             } else mp.events.call("AddNotify", "[Anti-Spam] Nur jede Sekunde möglich", "#c72020")
         });
 
-        mp.keys.bind(0x49, false, function() {
+        mp.keys.bind(0x49, true, function() {
             if (!player.loaded) return;
 
             if (interfaces.isOpen("Inventory")) {
@@ -31,6 +32,26 @@ class Keys {
                     mp.events.callRemote("Press", 0x49)
                     lastPressedI = now
                 } else mp.events.call("AddNotify", "[Anti-Spam] Nur jede 200ms möglich", "#c72020")
+            }
+        });
+
+        mp.keys.bind(0xBC, true, function() { // ,
+            if (!player.loaded || mp.players.local.vehicle != null) return;
+
+            const now = Date.now()
+            if (now - lastInteractKey > lastInteractCooldown) {
+                lastInteractKey = now
+                mp.events.callRemote("Comma")
+            }
+        });
+
+        mp.keys.bind(0xBE, true, function() { // .
+            if (!player.loaded || mp.players.local.vehicle != null) return;
+
+            const now = Date.now()
+            if (now - lastInteractKey > lastInteractCooldown) {
+                lastInteractKey = now
+                mp.events.callRemote("Dot")
             }
         });
 
